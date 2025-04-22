9b00122587c0e50394fcf6de8c349292d37dd97f
Júlia Moura <julia.araujo.moura.2020@gmail.com>
2025-04-22 09:17:54 -0300

    Adicionando exercicio 2

--- index.php
+++ index.php
@@ -1,53 +1,39 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercício 01</title>
+    <title>Exercício 02</title>
 </head>
-
 <body>
-    <h1>Exercício 01</h1>
+    <h1>Exercício 02</h1>
     <hr>
     <p><i>Faça as chamadas e testes nesta página</i></p>
-
-
-    <?php
-
-    // Importando a classe
-    require_once "src/Livro.php";
-
-
-    // Criando o Objeto
-    $livroA = new Livro();
-    $livroB = new Livro();
-
-    //Colocando valores nos objetos/atributos
-    $livroA->setTitulo("1984");
-    $livroA->setAutor("George orwell ");
-    $livroA->setPaginas(440);
-
-    $livroB->setTitulo("O Visconde Que Me Amava");
-    $livroB->setAutor("Julia Quinn");
-    $livroB->setPaginas(304);
-    ?>
-
-    <h2>Lendo as infromações</h2>
-
-    <ul>
-        <li><b>Título: </b> <?= $livroA->getTitulo() ?></li>
-        <li><b>Autor: </b> <?= $livroA->getAutor() ?></li>
-        <li><b>Páginas: </b> <?= $livroA->getPaginas() ?></li>
-    </ul>
-
-    <ul>
-        <li><b>Título: </b> <?= $livroB->getTitulo() ?></li>
-        <li><b>Autor: </b> <?= $livroB->getAutor() ?></li>
-        <li><b>Páginas: </b> <?= $livroB->getPaginas() ?></li>
-    </ul>
-
-
+    <hr>
+ 
+<?php
+//Importando a classe
+require_once "src/Livro.php";
+ 
+//Criando objeto da Classe
+$livroA = new Livro ("Amor & Gelato", "Jenna Evans Welch", 320);
+$livroB = new Livro ("A seleção", "Kiera Cass", 363);
+?>
+ 
+<h2>Lendo os dados do objeto</h2>
+ 
+<ul>
+    <li><b>Título: </b><?=$livroA->getTitulo()?></li>
+    <li><b>Autor: </b><?=$livroA->getAutor()?></li>
+    <li><b>N° de páginas: </b><?=$livroA->getPaginas()?></li>
+</ul>
+ 
+<ul>
+    <li><b>Título: </b><?=$livroB->getTitulo()?></li>
+    <li><b>Autor: </b><?=$livroB->getAutor()?></li>
+    <li><b>N° de páginas: </b><?=$livroB->getPaginas()?></li>
+</ul>
+ 
+   
 </body>
-
 </html>
--- src/Livro.php
+++ src/Livro.php
@@ -1,42 +1,50 @@
-<?php
-
+=<?php
 class Livro
 {
+    //Atributos/propriedades- (características do Livro)
     private string $titulo;
     private string $autor;
     private int $paginas;
-
-    // Adicionando metodo setters
-    public function setTitulo(string $titulo): void
+ 
+    //Método construtor
+    public function __construct(string $titulo, string $autor, int $paginas)
+    {
+        $this->setTitulo($titulo);
+        $this->setAutor($autor);
+        $this->setpaginas($paginas);
+    }
+ 
+    //Métodos setters
+    private function setTitulo(string $titulo): void
     {
         $this->titulo = $titulo;
     }
-
-    public function setAutor(string $autor): void
+ 
+    private function setAutor(string $autor): void
     {
         $this->autor = $autor;
     }
-
-    public function setPaginas(int $paginas): void
+   
+    private function setPaginas(string $paginas): void
     {
         $this->paginas = $paginas;
     }
-
-
-    // Adicioanndo metodo getters
-
-    public function getTitulo(): string
+ 
+    //Métodos getters
+    public function getTitulo():string
     {
-        return  $this->titulo;
+        return $this->titulo;
     }
-
-    public function getAutor(): string
+   
+    public function getAutor():string
     {
-        return  $this->autor;
+        return $this->autor;
     }
-
-    public function getPaginas(): int
+ 
+    public function getPaginas():int
     {
-        return  $this->paginas;
+        return $this->paginas;
     }
 }
+?>
+ 
